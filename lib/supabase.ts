5fee0eb7306f9a801399d8a6f526d48c078f352f
Efraim Alves <efraimrodrigoalves@gmail.com>
2026-04-10 16:01:02 -0300

    Adiciona vars do Supabase no app.json para APK

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,15 @@
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import { Constants } from 'expo-constants';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = Constants?.manifest?.extra?.supabaseUrl 
+  || process.env.EXPO_PUBLIC_SUPABASE_URL 
+  || 'https://fsyhunabticbctlxreff.supabase.co';
+
+const supabaseAnonKey = Constants?.manifest?.extra?.supabaseAnonKey 
+  || process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY 
+  || 'sb_publishable_9iRnVWwKIA5YYoau4Lc7xA_WAOlsPh6';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
